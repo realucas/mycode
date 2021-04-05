a554bb29a2633c669264e44eecd6c3dbab0d917a
realucas <realucas@outlook.com>
2021-04-05 20:22:11 +0000

    re-done listmeth02 added listmeth03

--- listmethods/listmeth02.py
+++ listmethods/listmeth02.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
-
-proto = ["ssh", "http",  "https"]
+proto = ["ssh", "http", "https"]
+protoa = ["ssh", "http", "https"]
 print(proto)
-print(f"this is using the f-strings and the protocols are the following: {proto[0]}, {proto[2]} are secure.") 
+proto.append("dns") # this line will add "dns" to the end of our list
+protoa.append("dns") # this line will add "dns" to the end of our list
+print(proto)
+proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+proto.extend(proto2) # pass proto2 as an argument to the extend method
+print(proto)
+protoa.append(proto2) # pass proto2 as an argument to the append method
+print(protoa)
 
---
+++ listmethods/listmeth03.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+proto = ["ssh", "http",  "https"]
+print(proto)
+print(f"this is using the f-strings and the protocols are the following: {proto[0]}, {proto[2]} are secure.") 
+
