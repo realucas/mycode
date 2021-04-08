f7a01b2b582f0cf5c4aabd79f1fd5c3497dbbf1b
realucas <realucas@outlook.com>
2021-04-08 18:26:59 +0000

    python and excel, including working with pandas

--- graphing/mygraph.py
+++ graphing/mygraph.py
@@ -3,14 +3,34 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from subprocess import call
 #import os
 
 #Pull filesystem utilization data from system, extract field for graph to txt or csv
 #def getfsstat():
     #os.system("df -h > dfstat.txt") or os.system("df -h | awk '{print $1, $5 }' > dfstat.txt")
+def getfsdata():
+    # issue our command to get filesystem usage
+    with open("dfstat.tmp", "w") as dfstat:
+        call(["df", "-h"], stdout=dfstat)
+    # open the file, read out data line by line
+    # remember, the first line is garbage! (stuff across the top)
+    linecount = 0
+    fsd = {}  # this is what we are going to return
+    with open("dfstat.tmp", "r") as dfstat:
+        for line in dfstat:
+            if linecount == 0: # if we're on the first line in our data
+                linecount += 1  # add 1 to linecount
+                continue       # skip to the next line... we only want lines 2 through n
+            line = line.split() # transform a STR into a LIST
+            fsd[f"fs{linecount}"] = {"name": line[0], "percent": line[4].rstrip("%"), "mount": line[5]}
+            #fsd[line[0]] = line[4].rstrip("%")  # fsd["udev"] = "0"
+            linecount += 1  # add 1 to linecount
+    return fsd
 
 
 def main():
+    print(getfsdata())
     # Fixing random state for reproducibility
     np.random.seed(19680801)
 
